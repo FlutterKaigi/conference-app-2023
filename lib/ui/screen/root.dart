a9a70a56d3cc3383208d43869cdf09de43a1de69
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-05-25 22:53:22 +0900

    Add NavigationDrawer

--- lib/ui/screen/root.dart
+++ lib/ui/screen/root.dart
@@ -16,7 +16,7 @@ enum RootTab {
   final String path;
 }
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({
     super.key,
     required this.navigator,
@@ -24,6 +24,11 @@ class RootScreen extends StatelessWidget {
 
   final Widget navigator;
 
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
   RootTab _getCurrentTab(BuildContext context) {
     final location = GoRouter.of(context).location;
     return RootTab.values.reversed.firstWhere(
@@ -40,7 +45,70 @@ class RootScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Tab: $currentTab'),
       ),
-      body: navigator,
+      drawer: NavigationDrawer(
+        selectedIndex: currentTab.index,
+        onDestinationSelected: (value) {
+          /// close drawer
+          Navigator.of(context).pop();
+
+          final nextTab = RootTab.values[value];
+          switch (nextTab) {
+            case RootTab.home:
+              const HomeRoute().go(context);
+            case RootTab.sessions:
+              const SessionsRoute().go(context);
+            case RootTab.sponsors:
+              const SponsorsRoute().go(context);
+            case RootTab.venue:
+              const VenueRoute().go(context);
+            case RootTab.contributors:
+              const ContributorsRoute().go(context);
+            case RootTab.settings:
+              const SettingsRoute().go(context);
+            case RootTab.license:
+              const LicenseRoute().go(context);
+          }
+        },
+        children: const [
+          Padding(
+            padding: EdgeInsets.all(24),
+            child: Text(
+              'Header',
+            ),
+          ),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Home'),
+          ),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Sessions'),
+          ),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Sponsors'),
+          ),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Venue'),
+          ),
+          Divider(),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Contributors'),
+          ),
+          Divider(),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Settings'),
+          ),
+          NavigationDrawerDestination(
+            icon: Icon(Icons.home),
+            label: Text('Licenses'),
+          ),
+        ],
+      ),
+      body: widget.navigator,
     );
   }
 }
