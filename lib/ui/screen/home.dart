93f733d7c9f6f9a8373c3b6e246e9762ed9d7717
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-06-02 01:47:45 +0900

    Add spacing property

--- lib/ui/screen/home.dart
+++ lib/ui/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:conference_2023/gen/assets.gen.dart';
+import 'package:conference_2023/util/extension/media_query_data_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -7,10 +8,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final margin = MediaQuery.of(context).spacing;
+
     return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(
+      padding: EdgeInsets.symmetric(
         vertical: 16,
-        horizontal: 16,
+        horizontal: margin,
       ),
       child: Column(
         children: [
