ad4230279e17f7f3761a9ff0f39e2b312f80da8f
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-06-02 01:39:11 +0900

    Add FlutterKaigi logo

--- lib/ui/screen/home.dart
+++ lib/ui/screen/home.dart
@@ -1,12 +1,30 @@
+import 'package:conference_2023/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Column(
+        children: [
+          const Gap(32),
+          Assets.svg.flutterkaigiLogo.svg(
+            width: 128,
+            height: 128,
+            colorFilter: const ColorFilter.mode(
+              Colors.transparent,
+              BlendMode.dst,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
