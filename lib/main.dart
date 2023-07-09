c9c37c4312a75b10a80d405aadc72db360f6cbc6
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-07-09 15:15:33 +0900

    Add firebase-core

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:conference_2023/app.dart';
 import 'package:conference_2023/model/preference/shared_preference_provider.dart';
 import 'package:conference_2023/util/font_lisence.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      /// FIXME: This is a temporary workaround for Firebase initialization
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyDCvtCUcu35-hK8AQF6GOkrmUuPQHAakHM',
+        appId: '1:1034811787358:web:6e39ea76ac0e44bd87cccf',
+        messagingSenderId: '1034811787358',
+        projectId: 'flutterkakgi-2023-conf-app-dev',
+        authDomain: 'flutterkakgi-2023-conf-app-dev.firebaseapp.com',
+        storageBucket: 'flutterkakgi-2023-conf-app-dev.appspot.com',
+      ),
+    );
+  } else {
+    /// Android, iOS
+    await Firebase.initializeApp();
+  }
 
   fontLicenses();
 
