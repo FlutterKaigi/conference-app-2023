ca711a254ec979b0a89c9bf433ddca573473c749
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-07-10 21:44:08 +0900

    fix

--- lib/util/web/firebase_options_web_dev.dart
+++ lib/util/web/firebase_options_web_dev.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const firebaseOptionsDev = FirebaseOptions(
-  apiKey: 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY',
+  apiKey: 'AIzaSyDCvtCUcu35-hK8AQF6GOkrmUuPQHAakHM',
   appId: '1:1034811787358:web:6e39ea76ac0e44bd87cccf',
   messagingSenderId: '1034811787358',
   projectId: 'flutterkakgi-2023-conf-app-dev',
