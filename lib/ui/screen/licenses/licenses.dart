ce19f28f1cedc96c4a1c89102642e57421c590a0
Kurogoma4D <highdro0479@gmail.com>
2023-08-02 20:11:22 +0900

    modify: migrate launch using uri

--- lib/ui/screen/licenses/licenses.dart
+++ lib/ui/screen/licenses/licenses.dart
@@ -1,9 +1,9 @@
 import 'package:conference_2023/l10n/localization.dart';
 import 'package:conference_2023/ui/router/router_app.dart';
 import 'package:conference_2023/util/extension/build_context_ext.dart';
+import 'package:conference_2023/util/launch_in_external_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class LicensesPage extends ConsumerWidget {
   const LicensesPage({super.key});
@@ -30,11 +30,7 @@ class LicensesPage extends ConsumerWidget {
           title: Text(localization.licensesPrivacyPolicy),
           onTap: () async {
             final url = Uri.parse(localization.licensesPrivacyPolicyUrl);
-            if (!await canLaunchUrl(url)) {
-              return;
-            }
-
-            await launchUrl(url);
+            await launchInExternalApp(url);
           },
         ),
         ListTile(
@@ -44,11 +40,7 @@ class LicensesPage extends ConsumerWidget {
           title: Text(localization.licensesCodeOfConduct),
           onTap: () async {
             final url = Uri.parse(localization.licensesCodeOfConductUrl);
-            if (!await canLaunchUrl(url)) {
-              return;
-            }
-
-            await launchUrl(url);
+            await launchInExternalApp(url);
           },
         ),
         ListTile(
@@ -58,11 +50,7 @@ class LicensesPage extends ConsumerWidget {
           title: Text(localization.licensesContactUs),
           onTap: () async {
             final url = Uri.parse(localization.licensesContactUsUrl);
-            if (!await canLaunchUrl(url)) {
-              return;
-            }
-
-            await launchUrl(url);
+            await launchInExternalApp(url);
           },
         ),
         ListTile(
