3198d855b73970c9611e165c512fa676608efd54
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-08-19 20:07:26 +0900

    Call usePathUrlStrategy

---
+++ lib/util/web/path_strategy.dart
@@ -0,0 +1,6 @@
+import 'package:conference_2023/util/web/path_strategy_io.dart'
+    if (dart.library.js) 'package:conference_2023/util/web/path_strategy_web.dart';
+
+void updatePathStrategy() {
+  changePathStrategy();
+}
---
+++ lib/util/web/path_strategy_io.dart
@@ -0,0 +1,3 @@
+void changePathStrategy() {
+  /// nop
+}
