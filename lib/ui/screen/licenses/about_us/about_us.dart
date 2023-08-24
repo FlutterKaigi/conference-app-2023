738c3bd678f23de41d14c9e0f7cf9672c74f9dbc
blendthink <admin@blendthink.dev>
2023-08-25 01:15:39 +0900

    feat: Implement the About us page

--- lib/ui/screen/licenses/about_us/about_us.dart
+++ lib/ui/screen/licenses/about_us/about_us.dart
@@ -1,6 +1,9 @@
+import 'package:conference_2023/gen/assets.gen.dart';
 import 'package:conference_2023/l10n/localization.dart';
+import 'package:conference_2023/util/extension/build_context_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 
 class AboutUsPage extends ConsumerWidget {
   const AboutUsPage({super.key});
@@ -8,12 +11,36 @@ class AboutUsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final localization = ref.watch(localizationProvider);
+    final theme = Theme.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(localization.licensesAboutUs),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: context.spacing),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(24),
+            Center(
+              child: Assets.svg.flutterkaigiLogo.svg(
+                width: 160,
+                height: 160,
+                colorFilter: const ColorFilter.mode(
+                  Colors.transparent,
+                  BlendMode.dst,
+                ),
+              ),
+            ),
+            const Gap(24),
+            Text(
+              localization.licensesAboutUsContents,
+              style: theme.textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
