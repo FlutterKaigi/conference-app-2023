df461f8489b96285ae4e029857b2c278882ee907
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-08-27 01:20:35 +0900

    Update libs

--- lib/model/permission.g.dart
+++ lib/model/permission.g.dart
@@ -7,12 +7,12 @@ part of 'permission.dart';
 // **************************************************************************
 
 String _$notificationPermissionHash() =>
-    r'5d5eece85573a3c67a5825a901b9f1be7e1aaaee';
+    r'b7b82f3fe25edb61d9ad1734fba8cfce2ea39568';
 
 /// See also [notificationPermission].
 @ProviderFor(notificationPermission)
 final notificationPermissionProvider =
-    AutoDisposeFutureProvider<NotificationPermission>.internal(
+    AutoDisposeFutureProvider<PermissionStatus>.internal(
   notificationPermission,
   name: r'notificationPermissionProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -23,6 +23,6 @@ final notificationPermissionProvider =
 );
 
 typedef NotificationPermissionRef
-    = AutoDisposeFutureProviderRef<NotificationPermission>;
+    = AutoDisposeFutureProviderRef<PermissionStatus>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
