f01247e64e4bce4de2ce064c8c1b37b3d54355b2
hironobu.iga <hironobu.iga.developer@gmail.com>
2023-09-10 18:54:33 +0900

    feat: add navigatorKeys

--- lib/ui/router/navigator_key.dart
+++ lib/ui/router/navigator_key.dart
@@ -2,3 +2,11 @@ import 'package:flutter/material.dart';
 
 final rootNavigatorKey = GlobalKey<NavigatorState>();
 final shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final homeNavigatorKey = GlobalKey<NavigatorState>();
+final sessionNavigatorKey = GlobalKey<NavigatorState>();
+final venueNavigatorKey = GlobalKey<NavigatorState>();
+final sponsorsNavigatorKey = GlobalKey<NavigatorState>();
+final contributorsNavigatorKey = GlobalKey<NavigatorState>();
+final settingsNavigatorKey = GlobalKey<NavigatorState>();
+final licenseNavigatorKey = GlobalKey<NavigatorState>();
