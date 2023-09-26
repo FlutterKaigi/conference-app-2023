6a7383cc6ec3ae40302a4d1cb04877176b5f6a62
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-09-27 00:00:10 +0900

    chore: coming soon

--- lib/ui/screen/venue/floor_map.dart
+++ lib/ui/screen/venue/floor_map.dart
@@ -5,6 +5,10 @@ class FloorMapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text(
+        'Coming soon',
+      ),
+    );
   }
 }
