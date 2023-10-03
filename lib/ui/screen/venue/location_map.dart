0b9461e884693901007dc236b2f89a9a2b794da3
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-10-03 21:46:42 +0900

    chore: Add coming soon

--- lib/ui/screen/venue/location_map.dart
+++ lib/ui/screen/venue/location_map.dart
@@ -5,6 +5,10 @@ class LocationMapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text(
+        'Coming soon',
+      ),
+    );
   }
 }
--- lib/ui/screen/venue/lunch_map.dart
+++ lib/ui/screen/venue/lunch_map.dart
@@ -5,6 +5,10 @@ class LaunchMapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text(
+        'Coming soon',
+      ),
+    );
   }
 }
