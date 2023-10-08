08318f638bf8f4b7d00a699725b5331e5d178ac4
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-10-08 21:00:53 +0900

    feat: Location map

--- lib/ui/screen/venue/location_map.dart
+++ lib/ui/screen/venue/location_map.dart
@@ -1,13 +1,69 @@
+import 'package:conference_2023/util/extension/build_context_ext.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_static_maps_controller/google_static_maps_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const _navitimeLatLng = GeocodedLocation.latLng(35.6672, 139.7150);
+final _navitimeMapUri =
+    Uri.parse('https://www.navitime.co.jp/maps/poi?code=02010-0000000001');
+final _googleMapUri = Uri.parse('https://maps.app.goo.gl/9ibs3TyTcgajHazE9');
 
 class LocationMapPage extends StatelessWidget {
   const LocationMapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Coming soon',
+    const maxSize = 600.0;
+
+    return Center(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: context.spacing,
+        ),
+        child: Column(
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: maxSize,
+              ),
+              child: Row(
+                children: [
+                  const Spacer(),
+                  OutlinedButton(
+                    onPressed: () async => launchUrl(_navitimeMapUri),
+                    child: const Text('Navitime Map'),
+                  ),
+                  const Gap(8),
+                  OutlinedButton(
+                    onPressed: () async => launchUrl(_googleMapUri),
+                    child: const Text('Google Map'),
+                  ),
+                ],
+              ),
+            ),
+            const Gap(16),
+            const StaticMap(
+              googleApiKey: String.fromEnvironment('google_map_api_key'),
+              width: maxSize,
+              height: maxSize,
+              zoom: 15,
+              center: _navitimeLatLng,
+              visible: [
+                _navitimeLatLng,
+              ],
+              markers: [
+                Marker(
+                  label: 'N',
+                  locations: [
+                    _navitimeLatLng,
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/ui/screen/venue/venue_dev_dummy.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class VenueDevDummy extends StatelessWidget {
+  const VenueDevDummy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Only production...',
+      ),
+    );
+  }
+}
