bcb857a714470d816e8ec870351d996d5378d228
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-10-09 15:51:16 +0900

    fix: Remove unncessary widgets

--- lib/ui/screen/venue/location_map.dart
+++ lib/ui/screen/venue/location_map.dart
@@ -13,62 +13,60 @@ class LocationMapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     const maxSize = 600.0;
 
-    return Center(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: context.spacing,
-        ),
-        child: Column(
-          children: [
-            ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: maxSize,
-              ),
-              child: Row(
-                children: [
-                  const Spacer(),
-                  OutlinedButton(
-                    onPressed: () async => launchInExternalApp(
-                      Uri.parse(
-                        'https://www.navitime.co.jp/maps/poi?code=02010-0000000001',
-                      ),
-                    ),
-                    child: const Text('NAVITIME Map'),
-                  ),
-                  const Gap(8),
-                  OutlinedButton(
-                    onPressed: () async => launchInExternalApp(
-                      Uri.parse(
-                        'https://maps.app.goo.gl/9ibs3TyTcgajHazE9',
-                      ),
-                    ),
-                    child: const Text('Google Map'),
-                  ),
-                ],
-              ),
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: context.spacing,
+      ),
+      child: Column(
+        children: [
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: maxSize,
             ),
-            const Gap(16),
-            const StaticMap(
-              googleApiKey: String.fromEnvironment('google_map_api_key'),
-              width: maxSize,
-              height: maxSize,
-              zoom: 15,
-              center: _navitimeLatLng,
-              visible: [
-                _navitimeLatLng,
-              ],
-              markers: [
-                Marker(
-                  label: 'N',
-                  locations: [
-                    _navitimeLatLng,
-                  ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(
+                  onPressed: () async => launchInExternalApp(
+                    Uri.parse(
+                      'https://www.navitime.co.jp/maps/poi?code=02010-0000000001',
+                    ),
+                  ),
+                  child: const Text('NAVITIME Map'),
+                ),
+                const Gap(8),
+                OutlinedButton(
+                  onPressed: () async => launchInExternalApp(
+                    Uri.parse(
+                      'https://maps.app.goo.gl/9ibs3TyTcgajHazE9',
+                    ),
+                  ),
+                  child: const Text('Google Map'),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          const Gap(16),
+          const StaticMap(
+            googleApiKey: String.fromEnvironment('google_map_api_key'),
+            width: maxSize,
+            height: maxSize,
+            zoom: 15,
+            center: _navitimeLatLng,
+            visible: [
+              _navitimeLatLng,
+            ],
+            markers: [
+              Marker(
+                label: 'N',
+                locations: [
+                  _navitimeLatLng,
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
