732b8fe7bc2749fdec495dc6a4d62e586e918c3a
koji-1009 <koji-1009@users.noreply.github.com>
2023-10-21 11:23:48 +0000

    deploy: 80ba2b9719c8626b08ab3d4643bc810b682114d5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "998d470191b2543ce3e10784624ce9d8",
-"/": "998d470191b2543ce3e10784624ce9d8",
+const RESOURCES = {"index.html": "1083618372f107ac843aa1f946a0fb6b",
+"/": "1083618372f107ac843aa1f946a0fb6b",
 "version.json": "72d4d604bf55ff2b46bcbbdb4e4dec4b",
 "images/d94625ae60d0db5775bf1dd89b5f254fb5fbc084.jpeg": "397099d5e5debecae88ccb8d3933604a",
 "images/5d964d4c-97c2-4fef-8449-483b879b9e7c.jpeg": "65a69942c979c8f159b1a6b88595f1eb",
@@ -91,7 +91,7 @@ const RESOURCES = {"index.html": "998d470191b2543ce3e10784624ce9d8",
 "icons/Icon-maskable-192.png": "a679793b3e27bbf310d4b4dc901c4cad",
 "icons/Icon-512.png": "ecae8f5e99edbe5f16f48cf7d14dfbec",
 "icons/Icon-192.png": "a679793b3e27bbf310d4b4dc901c4cad",
-"main.dart.js": "ffd4b8061b630c1a90c7bc84e8afbeb1"};
+"main.dart.js": "4908222fc126135c88bf62fd7b5cec8c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
