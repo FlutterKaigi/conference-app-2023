92230ebcf93c664923cd707133099726ae39c88a
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-21 16:05:22 +0900

    add: add providers to sort store list

--- lib/model/venue/lunch.dart
+++ lib/model/venue/lunch.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -28,4 +29,44 @@ class Store with _$Store {
   }) = _Lunch;
 
   factory Store.fromJson(Map<String, dynamic> json) => _$StoreFromJson(json);
+}
+
+extension StoreListEx on StoreList {
+  StoreList getSortedStoreListByOption(StoreSortOption sortOption) {
+    final copiedItems = [...items];
+    final topRow = copiedItems.firstWhereOrNull((e) => e.id == 'id');
+    if (topRow == null) {
+      return this;
+    }
+
+    copiedItems.removeWhere((e) => e.id == topRow.id);
+
+    if (sortOption.isById) {
+      copiedItems.sort(((a, b) => int.parse(a.id).compareTo(int.parse(b.id))));
+    } else if (sortOption.isAsc) {
+      copiedItems.sort(
+        ((a, b) => int.parse(a.routeTime).compareTo(int.parse(b.routeTime))),
+      );
+    } else if (sortOption.isDesc) {
+      copiedItems.sort(
+        ((a, b) => int.parse(b.routeTime).compareTo(int.parse(a.routeTime))),
+      );
+    }
+    return StoreList(items: [topRow, ...copiedItems]);
+  }
+}
+
+enum StoreSortOption {
+  byId('リセット'),
+  asc('昇順'),
+  desc('降順'),
+  ;
+
+  const StoreSortOption(this.label);
+
+  final String label;
+
+  bool get isById => this == StoreSortOption.byId;
+  bool get isAsc => this == StoreSortOption.asc;
+  bool get isDesc => this == StoreSortOption.desc;
 }
--- lib/model/venue/lunch_provider.dart
+++ lib/model/venue/lunch_provider.dart
@@ -19,3 +19,20 @@ StoreList storeList(
     ),
   );
 }
+
+@riverpod
+StoreList sortedStoreList(SortedStoreListRef ref) {
+  final storeList = ref.watch(storeListProvider);
+  final sortOption = ref.watch(storeSortOptionNotifierProvider);
+  return storeList.getSortedStoreListByOption(sortOption);
+}
+
+@riverpod
+class StoreSortOptionNotifier extends _$StoreSortOptionNotifier {
+  @override
+  StoreSortOption build() => StoreSortOption.byId;
+
+  Future<void> update(StoreSortOption sortOption) async {
+    state = sortOption;
+  }
+}
