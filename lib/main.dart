a47ba9a1f700cfb9c9434d3b2a263f60f80d19c2
Kurogoma4D <highdro0479@gmail.com>
2023-10-24 20:39:28 +0900

    modify: use root navigator for session detail

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,15 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   updatePathStrategy();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+
   await Firebase.initializeApp(
     options: firebaseOptionsWeb,
   );
--- lib/ui/screen/sessions/session_detail.dart
+++ lib/ui/screen/sessions/session_detail.dart
@@ -42,102 +42,107 @@ class SessionDetailPage extends ConsumerWidget {
       ),
     );
 
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(
-        horizontal: context.spacing,
-        vertical: 16,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(localization.pageTitleSessions),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  session.title.get(locale),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ),
-              IconButton(
-                icon: Icon(
-                  isFavorite ? Icons.favorite : Icons.favorite_border,
-                ),
-                onPressed: () async {
-                  isFavorite
-                      ? await ref
-                          .read(favoriteSessionIdsNotifierProvider.notifier)
-                          .remove(sessionId)
-                      : await ref
-                          .read(favoriteSessionIdsNotifierProvider.notifier)
-                          .add(sessionId);
-                },
-              ),
-            ],
-          ),
-          const Gap(8),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            padding: const EdgeInsets.symmetric(
-              vertical: 4,
-              horizontal: 8,
-            ),
-            child: Text(
-              room.alias,
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-            ),
-          ),
-          const Gap(16),
-          if (speaker != null) ...[
-            Wrap(
-              spacing: 16,
-              runSpacing: 8,
-              crossAxisAlignment: WrapCrossAlignment.center,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          horizontal: context.spacing,
+          vertical: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(speaker.avatarUrl),
+                Expanded(
+                  child: Text(
+                    session.title.get(locale),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
                 ),
-                Text(
-                  speaker.name,
-                  style: Theme.of(context).textTheme.bodyLarge,
+                IconButton(
+                  icon: Icon(
+                    isFavorite ? Icons.favorite : Icons.favorite_border,
+                  ),
+                  onPressed: () async {
+                    isFavorite
+                        ? await ref
+                            .read(favoriteSessionIdsNotifierProvider.notifier)
+                            .remove(sessionId)
+                        : await ref
+                            .read(favoriteSessionIdsNotifierProvider.notifier)
+                            .add(sessionId);
+                  },
                 ),
               ],
             ),
-            if (speaker.twitter.isNotEmpty) ...[
-              const Gap(8),
-              Tooltip(
-                message: localization.twitterTooltip,
-                child: ElevatedButton.icon(
-                  icon: Assets.svg.xLogo.svg(
-                    width: 18,
-                    colorFilter: ColorFilter.mode(
-                      Theme.of(context).colorScheme.primary,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                  label: Text('@${speaker.twitter}'),
-                  onPressed: () async {
-                    final uri = Uri.parse(
-                      'https://x.com/${speaker.twitter}',
-                    );
-                    await launchInExternalApp(uri);
-                  },
-                ),
+            const Gap(8),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(8),
               ),
-            ],
-            const Gap(24),
-          ],
-          if (description != null)
-            MarkdownBody(
-              shrinkWrap: true,
-              selectable: true,
-              data: description.get(locale),
+              padding: const EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 8,
+              ),
+              child: Text(
+                room.alias,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
             ),
-        ],
+            const Gap(16),
+            if (speaker != null) ...[
+              Wrap(
+                spacing: 16,
+                runSpacing: 8,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(speaker.avatarUrl),
+                  ),
+                  Text(
+                    speaker.name,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
+              ),
+              if (speaker.twitter.isNotEmpty) ...[
+                const Gap(8),
+                Tooltip(
+                  message: localization.twitterTooltip,
+                  child: ElevatedButton.icon(
+                    icon: Assets.svg.xLogo.svg(
+                      width: 18,
+                      colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.primary,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    label: Text('@${speaker.twitter}'),
+                    onPressed: () async {
+                      final uri = Uri.parse(
+                        'https://x.com/${speaker.twitter}',
+                      );
+                      await launchInExternalApp(uri);
+                    },
+                  ),
+                ),
+              ],
+              const Gap(24),
+            ],
+            if (description != null)
+              MarkdownBody(
+                shrinkWrap: true,
+                selectable: true,
+                data: description.get(locale),
+              ),
+          ],
+        ),
       ),
     );
   }
