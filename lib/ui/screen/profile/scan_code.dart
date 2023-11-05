4caf09e99c4924ea7acf04e5db23cb2ce3fcc6f8
Kurogoma4D <highdro0479@gmail.com>
2023-11-06 02:41:14 +0900

    modify: return scan result

--- lib/ui/screen/profile/scan_code.dart
+++ lib/ui/screen/profile/scan_code.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:conference_2023/model/profile/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -12,7 +15,19 @@ class ScanCodePage extends StatelessWidget {
       ),
       body: Center(
         child: MobileScanner(
-          onDetect: (capture) {},
+          onDetect: (capture) {
+            try {
+              final data = capture.barcodes.firstOrNull?.rawValue;
+              if (data == null) {
+                return;
+              }
+              final json = jsonDecode(data) as Map<String, dynamic>;
+              final profile = Profile.fromJson(json);
+              Navigator.of(context).pop(profile);
+            } catch (_) {
+              return;
+            }
+          },
           fit: BoxFit.contain,
         ),
       ),
